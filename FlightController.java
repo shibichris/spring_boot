8b71d364e51b7e77f221e8ded5d95c0737cdf0fd
shibichris <99465215+shibichris@users.noreply.github.com>
2023-04-17 10:00:25 +0530

    Update FlightController.java

--- FlightController.java
+++ FlightController.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired ;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
